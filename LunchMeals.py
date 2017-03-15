6bcfe5c80e89788e2e6cf11d76dc49e8902ef21c
lkpetrich <petrich@panix.com>
2017-03-14 23:23:06 -0700

    Update LunchMeals.py

--- LunchMeals.py
+++ LunchMeals.py
@@ -9,7 +9,7 @@
 # Bob loves hamburgers, likes dim sum, doesn't mind tapas, and hates pizza
 # Charlie loves hamburgers, likes tapas, doesn't mind dim sum and hates pizza
 # Dawn loves dim sum, likes pizza, doesn't mind tapas and hates hamburgers
-# Eve loves tapas, likes dim sum, doesn't mind pizza and hates hamburger
+# Eve loves tapas, likes dim sum, doesn't mind pizza and hates hamburgers
 
 from PrefVote import *
 
